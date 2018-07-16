1ca8de555de7602f4b3f8eac511093be9a0ad266
shelvadim <shelgachev@yahoo.com>
2018-07-16 14:34:46 -0700

    add user interface

--- public/js/index.js
+++ public/js/index.js
@@ -12,4 +12,26 @@
 
     socket.on('newMessage', function (message) {
         console.log('new email', message);
+         var li=jQuery('<li></li>');
+         li.text(`${message.from}:${message.text}`);
+
+         jQuery('#messages').append(li);
+    });
+
+    // socket.emit('createMessage', {
+    //     from:'Frank',
+    //     text:'Hi'
+    // }, function(data){
+    //     console.log('Ok', data);
+    // });
+
+    jQuery('#message-form').on('submit', function(e){
+        e.preventDefault();
+
+        socket.emit('createMessage', {
+            from: 'User',
+            text: jQuery('[name=message]').val()
+        }, function(){
+
+        });
     });
