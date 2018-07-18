b5742e571c678aa9ad518e59e09c015c4cb72197
shelvadim <shelgachev@yahoo.com>
2018-07-17 18:48:33 -0700

    add scroll messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
     var socket=io();
 
+    function scrollToBottom(){
+        //selectors
+        var messages=jQuery('#messages');
+        var newMessage=messages.children('li:last-child');
+        //heights
+        var clientHeight=messages.prop('clientHeight');
+        var scrollTop=messages.prop('scrollTop');
+        var scrollHeight=messages.prop('scrollHeight');
+        var newMessageHeight=newMessage.innerHeight();
+        var lastMessageHeight=newMessage.prev().innerHeight();
+
+        if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight){
+            messages.scrollTop(scrollHeight);
+        }
+    }
+
     socket.on('connect', function () {
         console.log('connected to server');
 
@@ -21,6 +37,7 @@
         });
 
         jQuery('#messages').append(html);
+        scrollToBottom();
         //console.log('new email', message);
           //  var li=jQuery('<li></li>');
         //  li.text(`${message.from} ${formatedTime}: ${message.text}`);
@@ -39,6 +56,7 @@
         });
 
         jQuery('#messages').append(html);
+        scrollToBottom();
 
         // var li=jQuery('<li></li>');
         // var a=jQuery('<a target="_blank">My current location</a>');
